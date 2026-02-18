21672a72e70e0bf7a28c0f60328f1b25a690e3f4
Lizzietrust <ohidaelizabeth3@gmail.com>
2026-02-18 11:35:22 +0100

    Add password hashing, comparison, and JWT generation methods to user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,6 +19,12 @@ const userSchema = new mongoose.Schema(
         "Please add a valid email",
       ],
     },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+      select: false,
+    },
     age: {
       type: Number,
       min: [0, "Age cannot be negative"],
@@ -26,15 +34,53 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: true,
     },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  { timestamps: true, collection: "users" },
+  {
+    timestamps: true,
+    collection: "users",
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.__v;
+        delete ret.password;
+        return ret;
+      },
+    },
+  },
 );
 
-// Instance method example
-userSchema.methods.toJSON = function () {
-  const user = this.toObject();
-  delete user.__v;
-  return user;
+// Hash password before saving
+userSchema.pre("save", async function () {
+  // Only hash if password is modified
+  if (!this.isModified("password")) {
+    return;
+  }
+
+  // Generate salt and hash password
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Method to generate JWT token
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      role: this.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE || "30d" },
+  );
 };
 
 // Static method example
