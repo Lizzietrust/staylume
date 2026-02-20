c1aa49e80899a70b00a36e0e291dc7599f25e607
Lizzietrust <ohidaelizabeth3@gmail.com>
2026-02-20 16:59:12 +0100

    feat: add home page components

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import HeroSection from "../components/home/HeroSection";
+import SearchBar from "../components/home/SearchBar";
+import PopularDestinations from "../components/home/PopularDestinations";
+import SpecialOffers from "../components/home/SpecialOffers";
+import TrendingCategories from "../components/home/TrendingCategories";
+import Newsletter from "../components/home/Newsletter";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroSection />
+      <SearchBar />
+      <PopularDestinations />
+      <SpecialOffers />
+      <TrendingCategories />
+      <Newsletter />
+    </div>
+  );
 };
 
 export default Home;
